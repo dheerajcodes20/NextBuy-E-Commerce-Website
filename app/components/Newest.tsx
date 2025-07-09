1a828fffb81dde4fc560ed9538cda414dbbaa015
dheerajcodes20 <dheeraj2082dk@gmail.com>
2025-07-09 21:36:58 +0530

    Changed the product handle click

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -26,12 +26,13 @@ export default async function Newest() {
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <div className="flex justify-between items-center ">
                     <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Newest Product</h2>
-                    <Link href="/all" className="text-primary flex items-center gap-x-l">
+                    <Link href="/all" className="text-primary flex items-center gap-x-l font-semibold hover:text-black">
                         See All{" "}<span><ArrowRight /></span>
                     </Link>
                 </div>
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {data.map((product) => (
+                        <Link href={`/product/${product.slug}`} >
                         <div key={product._id} className="group relative">
                             <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
                                 <Image
@@ -45,10 +46,10 @@ export default async function Newest() {
                             <div className="mt-4 flex justify-between">
                                 <div >
                                     <h3 className="text-sm text-gray-700">
-                                        <Link href={`/product/${product.slug}`} className="font- text-gray-900">
+                                        <div className=" text-gray-900">
                                             {product.title}
-
-                                        </Link>
+                                        </div>
+                                        
                                     </h3>
                                     <p className="mt-1 text-sm text-gray-500">{product.categoryName}
                                     </p>
@@ -56,6 +57,7 @@ export default async function Newest() {
                                 <p className="text-sm font-medium text-gray-900">₹{product.price}</p>
                             </div>
                         </div>
+                        </Link>
                     ))}
 
                 </div>
