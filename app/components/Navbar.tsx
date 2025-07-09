b5f5e42081e67c9df4955f020479777e361f08c0
dheerajcodes20 <dheeraj2082dk@gmail.com>
2025-07-09 21:12:02 +0530

    Changed nextbuy title  size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
       <div className='flex items-center justify-between mx-auto max-w-2xl px-4  sm:px-6 lg:max-w-7xl'>
         {/* Left side - Logo */}
         <Link href='/' >
-          <h1 className='text-xl sm:text-2xl md:text-4xl font-bold '>
+          <h1 className='text-3xl sm:text-2xl md:text-4xl font-bold '>
             Next<span className="text-primary">Buy</span>
           </h1>
         </Link>
